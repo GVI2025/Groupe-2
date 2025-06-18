90363dfc17a9c20ccd088b6b172e05963dc0e037
MatheoB62 <matheobultez62@gmail.com>
2025-06-18 09:33:14 +0200

    Fix: fix reservation service

--- app/models/reservationEntity.py
+++ app/models/reservationEntity.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column, String, Date, Time, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-import uuid
-
-Base = declarative_base()
+from uuid import uuid4
+from app.database.database import Base
 
 class Reservation(Base):
     __tablename__ = "reservations"
 
-    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = Column(String, primary_key=True, default=lambda: str(uuid4()))
     salle_id = Column(String, ForeignKey("salles.id"), nullable=False)
     date = Column(Date, nullable=False)
     heure = Column(Time, nullable=False)
